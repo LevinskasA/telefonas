01b4e7b4479c4af4578d5180c52c1aef6a8e363f
LevinskasA <andrelis97@gmail.com>
2017-02-15 20:52:29 +0200

    Starting

---
+++ Telefonai/Telefonai/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefonai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        Telefonas generuotiTelefona()
+        {
+
+        }
+    }
+
+    class OperacineSistema
+    {
+        string Pavadinimas { get; private set; }
+
+        public OperacineSistema(string pavadinimas)
+        {
+            Pavadinimas = pavadinimas;
+        }
+
+        public string GetVariantai(int variantas)
+        {
+            if (variantas == 1)
+            {
+                return "Android";
+            }
+            else if (true)
+            {
+                return "Apple iOS";
+            }
+            else if (true)
+            {
+                return "Windows";
+            }
+            else
+            {
+                return "OutOfOSBounds";
+            }
+        }
+    }
+}
